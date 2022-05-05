ff3f3f3a3e1b7b7e8c00d91f3c409bca01edc44b
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-05-04 20:01:25 -0400

    Slight changes to libdpncpp.h
    Ditto

--- include/libdpncpp.h
+++ include/libdpncpp.h
@@ -6,60 +6,62 @@
 
 namespace DPNCPP
 {
-    // TODO: prefix operators.
-    // TODO: More assignment operators.
+	// TODO: prefix operators.
+	// TODO: More assignment operators.
 
-    class DPN32
-    {
-        public:
-            DPN32 operator+(int32_t a);
-            DPN32 operator+(float a);
-            DPN32 operator+(DPN32 a);
-            DPN32 operator-(int32_t a);
-            DPN32 operator-(float a);
-            DPN32 operator-(DPN32 a);
-            DPN32 operator*(int32_t a);
-            DPN32 operator*(float a);
-            DPN32 operator*(DPN32 a);
-            DPN32 operator/(int32_t a);
-            DPN32 operator/(float a);
-            DPN32 operator/(DPN32 a);
-            DPN32 operator=(DPN32 a);
-            DPN32 operator=(int32_t a);
-            DPN32 operator=(float a);
-            DPN32 operator<<(int a);
-            DPN32 operator>>(int a);
-            void operator()(int a);
-            int32_t toInt();
-            float toFloat();
-        private:
-            DPN32_t core;
-    };
+	// Class representation of DPN32_t.
+	class DPN32
+	{
+		public:
+			DPN32 operator+(int32_t a);
+			DPN32 operator+(float a);
+			DPN32 operator+(DPN32 a);
+			DPN32 operator-(int32_t a);
+			DPN32 operator-(float a);
+			DPN32 operator-(DPN32 a);
+			DPN32 operator*(int32_t a);
+			DPN32 operator*(float a);
+			DPN32 operator*(DPN32 a);
+			DPN32 operator/(int32_t a);
+			DPN32 operator/(float a);
+			DPN32 operator/(DPN32 a);
+			DPN32 operator=(DPN32 a);
+			DPN32 operator=(int32_t a);
+			DPN32 operator=(float a);
+			DPN32 operator<<(int a);
+			DPN32 operator>>(int a);
+			void operator()(int a);
+			int32_t toInt();
+			float toFloat();
+		private:
+			DPN32_t core;
+	};
 
-    class DPN64
-    {
-        public:
-            DPN64 operator+(int64_t a);
-            DPN64 operator+(double a);
-            DPN64 operator+(DPN64 a);
-            DPN64 operator-(int64_t a);
-            DPN64 operator-(double a);
-            DPN64 operator-(DPN64 a);
-            DPN64 operator*(int64_t a);
-            DPN64 operator*(double a);
-            DPN64 operator*(DPN64 a);
-            DPN64 operator/(int64_t a);
-            DPN64 operator/(double a);
-            DPN64 operator/(DPN64 a);
-            DPN64 operator=(DPN64 a);
-            DPN64 operator=(int64_t a);
-            DPN64 operator=(double a);
-            DPN64 operator()(int a);
-            int64_t toInt();
-            double toDouble();
-        private:
-            DPN64_t core;
-    };
+	// Class representation of DPN64_t.
+	class DPN64
+	{
+		public:
+			DPN64 operator+(int64_t a);
+			DPN64 operator+(double a);
+			DPN64 operator+(DPN64 a);
+			DPN64 operator-(int64_t a);
+			DPN64 operator-(double a);
+			DPN64 operator-(DPN64 a);
+			DPN64 operator*(int64_t a);
+			DPN64 operator*(double a);
+			DPN64 operator*(DPN64 a);
+			DPN64 operator/(int64_t a);
+			DPN64 operator/(double a);
+			DPN64 operator/(DPN64 a);
+			DPN64 operator=(DPN64 a);
+			DPN64 operator=(int64_t a);
+			DPN64 operator=(double a);
+			DPN64 operator()(int a);
+			int64_t toInt();
+			double toDouble();
+		private:
+			DPN64_t core;
+	};
 }
 
 #endif
